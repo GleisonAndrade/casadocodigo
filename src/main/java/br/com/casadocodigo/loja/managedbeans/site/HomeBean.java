3e3e71fab7d9af0227a4a7fdbcc16cc159f780a3
Valney da Gama Costa <valney.gama@tce.pi.gov.br>
2016-08-05 14:51:56 -0300

    MONTE DE COISA

--- src/main/java/br/com/casadocodigo/loja/managedbeans/site/HomeBean.java
+++ src/main/java/br/com/casadocodigo/loja/managedbeans/site/HomeBean.java
@@ -2,20 +2,23 @@ package br.com.casadocodigo.loja.managedbeans.site;
 
 import java.util.List;
 
+import javax.enterprise.inject.Model;
 import javax.inject.Inject;
 
 import br.com.casadocodigo.loja.daos.BookDAO;
 import br.com.casadocodigo.loja.models.Book;
 
+@Model
 public class HomeBean {
+	
 	@Inject
 	private BookDAO bookDAO;
 	
 	public List<Book> olderBooks() {
-		return bookDAO.olders();
+		return bookDAO.olderBooks();
 	}
 	
-	public List<Book> releasedBooks() {
-		return bookDAO.released();
+	public List<Book> lastReleases() {
+		return bookDAO.lastReleases();
 	}
 }
